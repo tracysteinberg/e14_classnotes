874410f370d4db9fe2fadeb94c5e7680cbf99078
Keith Douglas <keithdouglas@MacBook-Pro-63.local>
2017-07-12 10:00:26 +0100

    Adds day 3 pizza_shop

--- week_03/day_3/pizza_shop/db/pizza_shop.sql
+++ week_03/day_3/pizza_shop/db/pizza_shop.sql
@@ -1,10 +1,9 @@
-DROP TABLE pizza_orders;
+DROP TABLE IF EXISTS pizza_orders;
 
 CREATE TABLE pizza_orders (
-  -- delete the first_name and last_name from pizza_orders
   id SERIAL4 PRIMARY KEY,
-  topping VARCHAR(255),
-  quantity INT2,
   first_name VARCHAR(255),
-  last_name VARCHAR(255)
+  last_name VARCHAR(255),
+  topping VARCHAR(255),
+  quantity INT2
 );

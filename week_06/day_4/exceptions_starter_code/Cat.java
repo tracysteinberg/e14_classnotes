a9ecce4b9fb63b88df468592c8ce1d4268b64faa
Keith Douglas <keithdouglas@MacBook-Pro-63.local>
2017-08-03 11:52:06 +0100

    Adds exceptions start code

---
+++ week_06/day_4/exceptions_starter_code/Cat.java
@@ -0,0 +1,12 @@
+public class Cat implements Pet{
+  private String name;
+
+  public Cat(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+}
---
+++ week_06/day_4/exceptions_starter_code/Fish.java
@@ -0,0 +1,12 @@
+public class Fish implements Pet{
+  private String name;
+
+  public Fish(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+  
+}
